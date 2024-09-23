90993debff052c868cc376188ed4608e5f07b444
KowshikGarapati <kowshikgarapati@gmail.com>
2024-09-23 13:24:55 +0000

    modified hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-4ju2n@$f9d0c=h)_g0lbb%k9&@rf(xa$d$g$&5ri$uf)*gev^4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".replit.dev", ".replit.app"]
-CSRF_TRUSTED_ORIGINS = ["https://*.replit.dev", "https://*.replit.app"]
+ALLOWED_HOSTS = [".replit.dev", ".replit.app", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://*.replit.dev", "https://*.replit.app", "http://localhost:3000", "http://127.0.0.1:3000"]
 
 # Application definition
 
